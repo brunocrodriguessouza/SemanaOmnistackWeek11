313a4606d17f502b4c8b5075c98f8384e9619ee5
Bruno Souza <brunoprogramadorjava@gmail.com>
2020-03-27 16:55:55 -0300

    [05] Garantindo os end points com Celebrate, framework que integra o joi com o express

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,6 @@
 const cors = require('cors');
 const express = require('express');
-
+const { errors } = require('celebrate');
 const routes = require('./routes');
 
 const app = express();
@@ -8,5 +8,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+app.use(errors());
 
 app.listen(3333);
